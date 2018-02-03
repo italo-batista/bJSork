89010d6b7c35a639a90529c2158a5508afa61555
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-02-03 11:42:37 -0300

    Crawling release date and track number

--- data/scripts/scripts/spiders/lyrics_spider.py
+++ data/scripts/scripts/spiders/lyrics_spider.py
@@ -3,11 +3,11 @@ import scrapy
 
 
 class LyricSpider(scrapy.Spider):
-    
+
     name = 'lyrics'
 
     start_urls = ['https://genius.com/albums/Bjork/Utopia',
-        'https://genius.com/albums/Bjork/Vulnicura',
+        'https://genius.com/albums/Bjork/Vulnicura'
         'https://genius.com/albums/Bjork/Biophilia',
         'https://genius.com/albums/Bjork/Volta',
         'https://genius.com/albums/Bjork/Medulla',
@@ -26,18 +26,36 @@ class LyricSpider(scrapy.Spider):
             yield scrapy.Request(href, callback=self.parse_album)
 
     def parse_album(self, response):
-        
+
         def extract_with_css(query):
             return response.css(query).extract_first().strip()
+
+        def extract_track_number():
+            resp = response.xpath(
+                '//span[contains(@class, "current")]/span/text()'
+            ).extract_first()
+
+            track_number = resp.split('.')[0].encode('utf-8').strip()
+            return track_number
 
         def extract_lyrics():
             lyrics = ""
             for line in response.css('.lyrics *::text').extract():
                 lyrics += line
-            
+
             return lyrics.replace("\n", " ").encode('utf-8').strip()
+
+        def extract_release_year():
+            resp = response.css(
+                'div span.metadata_unit-info.metadata_unit-info--text_only::text'
+            ).extract_first().encode('utf-8').strip()
+
+            return resp
 
         yield {
             'song': extract_with_css('.header_with_cover_art-primary_info-title::text'),
+            'track_number': extract_track_number(),
             'lyrics': extract_lyrics(),
+            'release_date': extract_release_year(),
+            'album': extract_with_css('a.song_album-info-title::text'),
         }
